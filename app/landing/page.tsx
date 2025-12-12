6765935d93da6208e7c12f4a0fbb86b28dec9960
Muhammad Usman Najmi <najmirashdalita12345@gmail.com>
2025-12-12 15:56:59 +0500

    fix: update import path for LoadingScreen component

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import axios from 'axios';
-import LoadingScreen from './components/LoadingScreen';
+import LoadingScreen from '../components/LoadingScreen';
 
 interface ClientConfig {
   httpsAppUrl: string;
